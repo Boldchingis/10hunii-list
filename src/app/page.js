d951a67e30f5f9505ffe57d33dc527e9c1ca82e7
Boldchingis <bboldchingis@gmail.com>
2024-11-28 16:40:59 +0800

    fdsfds

--- src/app/page.js
+++ src/app/page.js
@@ -1,126 +1,30 @@
 "use client";
 import "./style.css";
-
+import users from "./product.json";
 export default function Home() {
-  const productList = [
-    {
-      id: 1,
-      firstName: "Marian",
-      lastName: "Dixsee",
-      email: "mdixsee0@nba.com",
-      companyName: "Blogtags",
-      phoneNumber: "412-530-2249",
-      address: "62 Meadow Ridge Plaza",
-    },
-    {
-      id: 2,
-      firstName: "Coleman",
-      lastName: "Oxbie",
-      email: "coxbie1@oaic.gov.au",
-      companyName: "Fatz",
-      phoneNumber: "592-295-2012",
-      address: "18 Macpherson Place",
-    },
-    {
-      id: 3,
-      firstName: "Allx",
-      lastName: "Tomasian",
-      email: "atomasian2@cafepress.com",
-      companyName: "Abata",
-      phoneNumber: "741-828-1962",
-      address: "42040 Ridgeway Trail",
-    },
-    {
-      id: 4,
-      firstName: "Jarrett",
-      lastName: "Randales",
-      email: "jrandales3@wiley.com",
-      companyName: "Vidoo",
-      phoneNumber: "401-321-9934",
-      address: "595 Lakewood Street",
-    },
-    {
-      id: 5,
-      firstName: "Mora",
-      lastName: "Tildesley",
-      email: "mtildesley4@privacy.gov.au",
-      companyName: "Shuffletag",
-      phoneNumber: "254-913-7925",
-      address: "7 Little Fleur Point",
-    },
-    {
-      id: 6,
-      firstName: "Livvyy",
-      lastName: "Crankhorn",
-      email: "lcrankhorn5@google.com.br",
-      companyName: "Fivespan",
-      phoneNumber: "589-951-2909",
-      address: "23524 Thackeray Street",
-    },
-    {
-      id: 7,
-      firstName: "Erwin",
-      lastName: "LLelweln",
-      email: "ellelweln6@de.vu",
-      companyName: "Cogibox",
-      phoneNumber: "105-760-3548",
-      address: "20 Cardinal Pass",
-    },
-    {
-      id: 8,
-      firstName: "Jourdain",
-      lastName: "Leban",
-      email: "jleban7@is.gd",
-      companyName: "Layo",
-      phoneNumber: "124-860-5889",
-      address: "77 Fair Oaks Court",
-    },
-    {
-      id: 9,
-      firstName: "Christy",
-      lastName: "Van Der Straaten",
-      email: "cvanderstraaten8@dailymail.co.uk",
-      companyName: "Linktype",
-      phoneNumber: "460-169-0338",
-      address: "94749 Mendota Circle",
-    },
-    {
-      id: 10,
-      firstName: "Jehanna",
-      lastName: "Cleobury",
-      email: "jcleobury9@huffingtonpost.com",
-      companyName: "Flashpoint",
-      phoneNumber: "396-811-4137",
-      address: "3759 Oak Valley Park",
-    },
-  ];
-
-  const userList = productList.map((user) => (
-    <div key={user.id} className="card">
+  const userList = users.map((props) => (
+    <div key={props.id} className="card">
       <div className="leftSide">
         {" "}
         <h2>
-          {user.firstName} {user.lastName}
+          {props.firstName} {props.lastName}
         </h2>
         <p>
-          <strong>Phone:</strong> {user.phoneNumber}
+          <strong>Number :</strong> {props.phoneNumber}
         </p>
         <p>
-          <strong>Email:</strong> {user.email}
+          <strong>Email:</strong> {props.email}
         </p>
         <p>
-          <strong>Address:</strong> {user.address}
+          <strong>Address:</strong> {props.address}
         </p>{" "}
       </div>
       <div className="rightSide">
-        {/* <p>
-        <strong>Company:</strong> {user.companyName}
-      </p> */}
-        {/* <img src="Logo000 1.png"></img> */}
+        <div className="company"> {props.companyName} </div>
+        <img className="logo" src="Logo000 1.png"></img>
         <img className="rightLogo" src="Shape.png"></img>
       </div>
     </div>
   ));
-
   return <div>{userList}</div>;
 }
